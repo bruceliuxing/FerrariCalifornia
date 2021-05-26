115368f1cc73cb8b5a5408c95da026a6096bcfe6
bruceliuxing <840193139@qq.com>
2021-05-26 15:15:18 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -63,7 +63,15 @@
 
  def md5(string):
      return hashlib.md5(string).hexdigest()
-    
+ 
+ def process_bar(percent, start_str='', end_str='100%', total_length=50):
+     bar = ''.join(["\033[31m%s\033[0m"%'   '] * int(percent * total_length)) + ''
+     bar = '\r' + start_str + bar.ljust(total_length) + ' {:0>4.4f}%|'.format(percent*100) + end_str
+     sys.stdout.write(bar)
+     sys.stdout.flush()
+ #    print(bar, end='', flush=True)
+ 
+ 
  def cv_imread_check(image_path):
      """
      obtain valid image RGB format data
